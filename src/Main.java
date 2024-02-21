05c9cb06b20c296e80ca31a1911c800838ae157c
sertackıncal <mehmetsertackincal945@gmail.com>
2024-02-21 22:12:42 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import  java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int a,b,c;
+        a=10;
+        b=2;
+        c=3;
+        Scanner input = new Scanner(System.in);
+        //Declare variables.
+        int result = a+b*c-b;
+        System.out.println("result: " + result );
+    }
+}
+
